d1061b5bc627fe6690a0f2b1e0874f90ca51b276
ahmadramadhannn <ahmadoneperson@gmail.com>
2025-11-22 19:19:30 +0700

    starting out with flutter and spring boot

---
+++ server/src/main/java/com/ahmadramadhan/mudahtitip/MudahtitipApplication.java
@@ -0,0 +1,13 @@
+package com.ahmadramadhan.mudahtitip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MudahtitipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MudahtitipApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/ahmadramadhan/mudahtitip/MudahtitipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahmadramadhan.mudahtitip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MudahtitipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
